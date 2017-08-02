78a903e1e779380116e0a83daf8ae134ecc6106f
“Your <apurvdeshpande91@gmail.com>
2017-08-02 12:38:49 +0100

    Dispalying Recipes

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,12 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from '../recipe.model';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  recipes =[];
+  recipes: Recipe[] =[
+    new Recipe('A Test Recipe', 'This is simply a test', 'http://images.media-allrecipes.com/userphotos/250x250/00/93/15/931547.jpg')
+  ];
   constructor() { }
 
   ngOnInit() {
