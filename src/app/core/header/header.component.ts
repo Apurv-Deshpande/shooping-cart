9f9e444aaf90b2b288c5761f091191122100ab0d
“Your <apurvdeshpande91@gmail.com>
2017-08-25 11:22:08 +0100

    Adding Home Page and Cleaning Up

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -4,13 +4,14 @@ import { Response } from '@angular/http';
 import { DataStorageService } from '../../shared/data-storage.service';
 import { AuthService } from '../../auth/auth.service';
 
-
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html'
 })
 export class HeaderComponent {
-  constructor(private dataStorageService: DataStorageService, private authService: AuthService) {}
+  constructor(private dataStorageService: DataStorageService,
+              private authService: AuthService) {
+  }
 
   onSaveData() {
     this.dataStorageService.storeRecipes()
@@ -25,8 +26,11 @@ export class HeaderComponent {
     this.dataStorageService.getRecipes();
   }
 
-
   onLogout() {
     this.authService.logout();
+  }
+
+  isAuthenticated() {
+    return this.authService.isAuthenticated();
   }
 }
